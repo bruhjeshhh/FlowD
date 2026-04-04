87959e7d355b97182e24f66b76e476a3de5e4431
brajesh <mohantybrajesh4@gmail.com>
2026-04-04 16:09:55 +0530

    fix: bugs in initial migration

--- migrations/schema/001_jobs.sql
+++ migrations/schema/001_jobs.sql
@@ -1,11 +1,11 @@
 -- +goose up
 create table jobs(id uuid primary key,
 payload json not null,
- Status ENUM('Pending', 'Processing','Success','Failed') ,
+ status TEXT CHECK (status IN ('pending', 'processing','success','failed'))
 retry_count int not null default 0,
 max_retries int not null default 3,
 idempotency_key varchar not null unique,
-scheduled_at timestamp  default now(),
+scheduled_at timestamp,
 created_at timestamp not null default now(),
 updated_at timestamp not null default now());
 -- +goose down 
