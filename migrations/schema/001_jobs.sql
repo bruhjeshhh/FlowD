bad0e56d84b4e4abec552d6a29f83744643f296c
brajesh <mohantybrajesh4@gmail.com>
2026-04-04 16:28:29 +0530

    fix: sql syntax bug

--- migrations/schema/001_jobs.sql
+++ migrations/schema/001_jobs.sql
@@ -1,7 +1,7 @@
 -- +goose up
 create table jobs(id uuid primary key,
 payload json not null,
- status TEXT CHECK (status IN ('pending', 'processing','success','failed'))
+ status TEXT CHECK (status IN ('pending', 'processing','success','failed')),
 retry_count int not null default 0,
 max_retries int not null default 3,
 idempotency_key varchar not null unique,
