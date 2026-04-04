02f168919ba7cfb2fb17fd9d49cf318e9ea9d32c
brajesh <mohantybrajesh4@gmail.com>
2026-04-04 18:21:10 +0530

    chore: add basic validating

--- jobs.go
+++ jobs.go
@@ -1,5 +1,43 @@
 package main
 
-func valiatepayload()
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
 
-func insertjob()
+type incoming struct {
+	Payload struct {
+		Type string `json:"type"`
+		Data struct {
+			To      string `json:"to"`
+			Subject string `json:"subject"`
+			Body    string `json:"body"`
+		} `json:"data"`
+	} `json:"payload"`
+	ScheduledAt    time.Time `json:"scheduled_at"`
+	IdempotencyKey string    `json:"idempotency_key"`
+}
+
+func validatePayload(pls incoming) bool {
+	if pls.Payload.Type == "" {
+		return false
+	}
+	return true
+}
+
+func (c *apiConfig) insertjob(w http.ResponseWriter, r *http.Request) {
+	decode := json.NewDecoder(r.Body)
+	pld := incoming{}
+	decodingerror := decode.Decode(&pld)
+	if decodingerror != nil {
+		respondWithError(w, 400, "not a Json(probably)")
+	}
+	validity := validatePayload(pld)
+
+	if validity == false {
+		respondWithError(w, 400, "invalid format")
+		return
+	}
+
+}
