0b2681ffcd73f3ec18e4312fc16ea59ce24094fe
brajesh <mohantybrajesh4@gmail.com>
2026-04-04 16:56:34 +0530

    chore: add sqlc + function to insert a job into the db

---
+++ migrations/queries/jobs.sql
@@ -0,0 +1,14 @@
+-- name: insertjob :one
+INSERT INTO jobs(id, payload, status, retry_count, max_retries, idempotency_key, scheduled_at,  created_at, updated_at)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8,
+    $9
+)
+RETURNING *;
